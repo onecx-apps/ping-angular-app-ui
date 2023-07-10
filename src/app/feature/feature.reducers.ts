9e7047c36f0f89f7ebad78fdc8eaa794e945d16f
Jan-Gerrit Schettler-Köhler <jan-gerrit.schettler-koehler@capgemini.com>
2023-07-10 09:29:18 +0200

    feat: fixed prettier issues

--- src/app/feature/feature.reducers.ts
+++ src/app/feature/feature.reducers.ts
@@ -1,15 +1,10 @@
-import {
-  combineReducers,
-  createFeature,
-} from '@ngrx/store';
+import { combineReducers, createFeature } from '@ngrx/store';
 import { FeatureState } from './feature.state';
 import { entitiesSearchReducer } from './pages/entity-search/entity-search.reducer';
 
-  export const featureFeature = createFeature({
-    name: 'feature',
-    reducer: combineReducers<FeatureState>({
-      entitySearch: entitiesSearchReducer,
-    })
-  });
-
-  
+export const featureFeature = createFeature({
+  name: 'feature',
+  reducer: combineReducers<FeatureState>({
+    entitySearch: entitiesSearchReducer,
+  }),
+});
